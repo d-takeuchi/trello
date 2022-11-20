f357c2e8e5c500ed4a5993803fd6d4a25688fcf1
d-takeuchi <kuronk.kuro3@gmail.com>
2022-11-20 21:48:52 +0900

    mod: リストの一覧表示処理の追加

--- src/components/BoardPage.vue
+++ src/components/BoardPage.vue
@@ -3,19 +3,33 @@
     <header>Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
-      <ListAdd />
+      <div class="list-index">
+        <ListComponent
+          v-for="(item, index) in lists"
+          :key="item.id"
+          :title="item.title"
+          :listIndex="index"
+        />
+
+        <ListAdd />
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 import ListAdd from './ListAdd.vue'
+import ListComponent from './ListComponent.vue'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     ListAdd,
+    ListComponent,
+  },
+  computed: {
+    //storeのstateに定義したstateを呼び出す記述
+    ...mapState(['lists']),
   },
 }
 </script>
-
-<style></style>
